79f25d1b483a0befc01aa32fbc9b7e344d4358a9
ytekim <mdmelo@users.noreply.github.com>
2025-06-29 11:01:41 +0000

    Enable Flask Development Mode

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-from app import app
+from app import create_app
+import os
 
 if __name__ == '__main__':
+    # Force development mode
+    os.environ['FLASK_ENV'] = 'development'
+    
+    # Create app in development mode
+    app = create_app('development')
+    
+    # Run with debug enabled
     app.run(host='0.0.0.0', port=5000, debug=True)
